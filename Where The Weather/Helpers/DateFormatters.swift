b20e3e2f126786b461036c1d0aaefdbdbf29816e
Alex Agapov <agapov.one@gmail.com>
2017-04-15 18:24:50 +0500

    Change date format

--- Where The Weather/Helpers/DateFormatters.swift
+++ Where The Weather/Helpers/DateFormatters.swift
@@ -11,7 +11,7 @@ import Foundation
 extension DateFormatter {
     class var dayFormatter: DateFormatter {
         let formatter = DateFormatter()
-        formatter.dateFormat = "EEEE"
+        formatter.dateFormat = "EEEE, MMMM dd"
         return formatter
     }
 }
